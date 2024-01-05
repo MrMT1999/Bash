031f057528d402d1bfe1f98bfea784091463ab1b
MrMT1999 <146210254+MrMT1999@users.noreply.github.com>
2024-01-05 23:07:39 +0330

    Create Password.sh
    the first version which can just hold the passwords in a text file

---
+++ Password.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get input string from the user
+read -p "Enter The Service: " input_string
+
+# Generate a random password in 8 characters
+password=$(openssl rand -base64 12 | head -c 8)
+
+# Will showw the string and password
+echo "Service: $input_string"
+echo "Password: $password"
+
+# Store the string and password in a text file
+echo "String: $input_string" >> storage.txt
+echo "Password: $password" >> storage.txt
+
+echo "Data stored in storage.txt"
